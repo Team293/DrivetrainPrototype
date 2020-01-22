1fc27b633d0296279cb38e23235a8d51fcbfddc8
Brian Shen <brianxshen@gmail.com>
2020-01-22 12:46:08 -0800

    Working leadscrew elevator for climb \

--- src/main/java/frc/robot/commands/ArmUp.java
+++ src/main/java/frc/robot/commands/ArmUp.java
@@ -49,17 +49,20 @@ public class ArmUp extends Command {
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
+       
         return false;
     }
 
     // Called once after isFinished returns true
     @Override
     protected void end() {
+        Robot.climber.ArmStop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     @Override
     protected void interrupted() {
+        Robot.climber.leadScrew.set(0);
     }
 }
