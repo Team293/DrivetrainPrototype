3068941fc8a0e786a972ba012391c82e73cd0d02
BobTheLegend <45445483+BobTheLegend@users.noreply.github.com>
2020-01-29 15:04:36 -0500

    Shooter revamped all commands have been fixed but still untested bc of max

--- src/main/java/frc/robot/commands/ShootOn.java
+++ src/main/java/frc/robot/commands/ShootOn.java
@@ -43,6 +43,7 @@ public class ShootOn extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+        Robot.shooter.shoot(true,Robot.oi.getrightJoy().getThrottle());
     }
 
     // Make this return true when this Command no longer needs to run execute()
